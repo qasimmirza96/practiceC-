46a185c9ec521c700fc2f351bbe8aaf1dd95afe1
qasimmirza96 <freeused4@gmail.com>
2025-05-25 22:47:49 +0500

    Solved puzzle to find even numbers and reverse the array

--- obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dc2d753215e09cee18893ee8ca980cb7e20a689")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- program.cs
+++ program.cs
@@ -1,1 +1,51 @@
-﻿
+﻿using System;
+class Program
+{
+	/*
+		🔧 Instructions:
+		1. Ask the user to enter 8 numbers.
+		2. Store them in an array.
+		3. Create a second array that stores only the **even numbers** in **reverse order**.
+		4. Then print the second array.
+
+	*/
+	static void Main()
+	{
+		int[] numbers = new int[8];
+		for (int i = 0; i < numbers.Length; i++)
+		{
+			Console.WriteLine($"Enter number for index {i + 1}: ");
+			numbers[i] = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine($"Entered number for index {i + 1} is: {numbers[i]} ");
+		}
+		int evenNumbers = 0;
+		for (int i = 0; i < numbers.Length; i++)
+		{
+			if (numbers[i] % 2 == 0)
+			{
+				evenNumbers++;
+			}
+		}
+		int[] secondArray = new int[evenNumbers];
+		int j = 0;
+		for (int i = numbers.Length - 1; i >= 0; i--)
+		{
+			if (numbers[i] % 2 == 0)
+			{
+
+				secondArray[j] = numbers[i];
+				j++;
+			}
+
+		}
+		Console.WriteLine("\nEven numbers in reverse order:");
+		for (int i = 0; i < secondArray.Length; i++)
+		{
+
+			Console.WriteLine($"Entered number for index {i + 1} is: {secondArray[i]} ");
+		}
+
+
+
+	}
+}
